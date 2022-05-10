6db85e74077925671e2016a2bce76fc29e88d733
Mattefarax <matteo.faraoni@stud.tecnicosuperiorekennedy.it>
2022-05-10 15:28:35 +0200

    added throw messages

--- green/src/main/java/Utils.kt
+++ green/src/main/java/Utils.kt
@@ -1,23 +1,23 @@
 import java.io.File
 import java.io.FileNotFoundException
+import java.io.IOException
 import java.net.URL
 
 object Utils {
 
-     fun downloadFile(url: String, path: String): File {
+    fun downloadFile(url: String, path: String): File {
         val file = File(path)
-        file.createNewFile()
-        val inputStream = try {
-            URL(url)
-        } catch (ex: FileNotFoundException) {
-            println("Download failed! File $url doesn't exists")
-            null
-        }
-        inputStream?.let {
-            it.openStream().use {  input ->
+        try {
+            file.createNewFile()
+            URL(url).openStream().use {  input ->
                 file.outputStream().use { output -> input.copyTo(output) }
             }
+        } catch (ex: FileNotFoundException) {
+            throw FileNotFoundException("Download failed! File $url doesn't exists")
+        } catch (ex: IOException) {
+            throw IOException("Unable to create ${file.name} file")
         }
         return file
     }
+
 }
